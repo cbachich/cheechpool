ec6ad079340ac8dc4d03a32cb89f495036b83fa4
Christopher Bachicha <cbachich@gmail.com>
2012-10-25 08:55:17 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.6'
+  gem 'rspec-rails', '2.11.4'
+end
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.3.0'
+end
+
+gem 'jquery-rails', '2.1.3'
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+group :production do
+  gem 'pg', '0.14.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheechPool::Application.config.secret_token = 'cb40ef6d1a716254f8d265fc45aa385ccabef42a189147bf890fcff1b5eca7792fbe75c846d9a012b5cd9bba97047fafa4a46f97ff1ef98046eb15ea7ff9b715'
