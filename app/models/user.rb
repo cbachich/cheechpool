e30a1eb4f70e50fa6ee38f3fc91fccf9e0c88d5c
Christopher Bachicha <cbachich@gmail.com>
2012-11-08 09:10:09 -0700

    Update password authentication to be more secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,29 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, 
+                  :name, 
+                  :password, 
+                  :password_confirmation
+  has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   before_save { |user| user.email = email.downcase }
 
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  
+            presence: true, 
+            length: { maximum: 50 }
+
   validates :email, 
             presence: true, 
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+
+  validates :password, 
+            presence: true,
+            length: { minimum: 6 }
+
+  validates :password_confirmation,
+            presence: true
 end
