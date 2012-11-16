ee901de07d7eb328f7c8f5ce3c58a3f87059a05a
Christopher Bachicha <cbachich@gmail.com>
2012-11-15 19:37:02 -0700

    Add signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,15 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 end
