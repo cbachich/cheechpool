61d84eebcab2fee5c1cc6b22146762885f65e32c
Christopher Bachicha <cbachich@gmail.com>
2012-11-20 08:22:11 -0700

    Add signin failure flash with page refresh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # Sign in the user and redirect to their page
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
