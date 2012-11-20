ff861ca41285129f46963a69ba61308db16efe49
Christopher Bachicha <cbachich@gmail.com>
2012-11-20 08:35:15 -0700

    Add a remember token for remembering a user's session

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name,  
             presence: true, 
@@ -35,4 +36,10 @@ class User < ActiveRecord::Base
 
   validates :password_confirmation,
             presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
