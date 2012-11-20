ba453943bb881650daea95e5932db1d01b76ab00
Christopher Bachicha <cbachich@gmail.com>
2012-11-20 09:22:40 -0700

    Add signout functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,11 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
   def signed_in?
     !current_user.nil?
   end
