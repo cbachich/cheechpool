702c2c0c7d731938808750e2e4d42a962a631e9b
Christopher Bachicha <cbachich@gmail.com>
2012-12-12 09:22:49 -0700

    Add pagination to the users index

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Christopher Bachicha"
-    email    "chris@cheechpool.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.org" }
     password "foobar"
     password_confirmation "foobar"
   end
