2aca323f8e1e80000ca21a22a794f12d243eeffb
Christopher Bachicha <cbachich@gmail.com>
2013-01-07 09:05:56 -0700

    Create a league model

---
+++ spec/models/league_spec.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: leagues
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe League do
+
+  before { @league = League.new(name: "Example League") }
+
+  subject { @league }
+
+  it { should respond_to(:name) }
+end
