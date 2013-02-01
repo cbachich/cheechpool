a4bca3398366f0c7b8e4682cdb29ae42f3f933d4
Christopher Bachicha <cbachich@gmail.com>
2013-02-01 15:21:49 -0700

    Update the picks generation script to incorporate weeks

--- script/gen_picks.rb
+++ script/gen_picks.rb
@@ -8,64 +8,82 @@ brandon = Player.find_by_name("Brandon")
 laura = Player.find_by_name("Laura")
 sher = Player.find_by_name("Sherri")
 
+##################
+# Player Results #
+##################
+laura.voted_out_week = 1
+laura.save
+andrea.voted_out_week = 2
+andrea.save
+
 ################
 # Cheech Picks #
 ################
 
-# Week 1 - Team Choice - Gota
-cheech.team_picks.create(league_id: survivor.id, week: 1, picked: true, team_id: gota.id)
-cheech.team_picks.create(league_id: survivor.id, week: 1, picked: false, team_id: bikal.id)
+# --------- Week #1 ------------------------
+cw1 = cheech.weeks.create(league_id: survivor.id, number: 1, score: 0)
 
-# Week 2 - Team Choice - Bikal
-cheech.team_picks.create(league_id: survivor.id, week: 2, picked: false, team_id: gota.id)
-cheech.team_picks.create(league_id: survivor.id, week: 2, picked: true, team_id: bikal.id)
+# Team Choice - Gota
+cw1.team_picks.create(team_id: gota.id,  picked: true)
+cw1.team_picks.create(team_id: bikal.id, picked: false)
 
-# Week 3 - Team Choice - Gota
-cheech.team_picks.create(league_id: survivor.id, week: 3, picked: true, team_id: gota.id)
-cheech.team_picks.create(league_id: survivor.id, week: 3, picked: false, team_id: bikal.id)
+# Player Values - Laura voted out
+cw1.player_picks.create(player_id: andrea.id,  value: 1)
+cw1.player_picks.create(player_id: brandon.id, value: 2)
+cw1.player_picks.create(player_id: laura.id,   value: 3)
+cw1.player_picks.create(player_id: sher.id,    value: 4)
 
-# Week 1 - Player Values - Laura voted out
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 1, player_id: andrea.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 2, player_id: brandon.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 3, player_id: laura.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 4, player_id: sher.id)
+# --------- Week #2 ------------------------
+cw2 = cheech.weeks.create(league_id: survivor.id, number: 2, score: 0)
 
-# Week 2 - Player Values - Andrea voted out
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 2, player_id: andrea.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 3, player_id: brandon.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 1, player_id: sher.id)
+# Team Choice - Bikal
+cw2.team_picks.create(team_id: gota.id,  picked: false)
+cw2.team_picks.create(team_id: bikal.id, picked: true)
 
-# Week 3 - Player Values - Sher voted out
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 1, player_id: brandon.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 2, player_id: sher.id)
+# Player Values - Andrea voted out
+cw2.player_picks.create(player_id: andrea.id,  value: 3)
+cw2.player_picks.create(player_id: brandon.id, value: 1)
+cw2.player_picks.create(player_id: sher.id,    value: 2)
+
+# --------- Week #3 ------------------------
+cw3 = cheech.weeks.create(league_id: survivor.id, number: 3, score: 0)
+
+# Player Choice - Sher
+cw3.player_picks.create(player_id: brandon.id, picked: false)
+cw3.player_picks.create(player_id: sher.id,    picked: true)
 
 #############
 # Bob Picks #
 #############
 
-# Week 1 - Team Choice - Bikal
-bob.team_picks.create(league_id: survivor.id, week: 1, picked: false, team_id: gota.id)
-bob.team_picks.create(league_id: survivor.id, week: 1, picked: true, team_id: bikal.id)
+# --------- Week #1 ------------------------
+bw1 = bob.weeks.create(league_id: survivor.id, number: 1, score: 0)
 
-# Week 2 - Team Choice - Bikal
-bob.team_picks.create(league_id: survivor.id, week: 2, picked: false, team_id: gota.id)
-bob.team_picks.create(league_id: survivor.id, week: 2, picked: true, team_id: bikal.id)
+# Team Choice - Bikal
+bw1.team_picks.create(team_id: gota.id,  picked: false)
+bw1.team_picks.create(team_id: bikal.id, picked: true)
 
-# Week 3 - Team Choice - Bikal
-bob.team_picks.create(league_id: survivor.id, week: 3, picked: false, team_id: gota.id)
-bob.team_picks.create(league_id: survivor.id, week: 3, picked: true, team_id: bikal.id)
+# Player Values - Laura voted out
+bw1.player_picks.create(player_id: andrea.id,  value: 4)
+bw1.player_picks.create(player_id: brandon.id, value: 2)
+bw1.player_picks.create(player_id: laura.id,   value: 1)
+bw1.player_picks.create(player_id: sher.id,    value: 3)
 
-# Week 1 - Player Values - Laura voted out
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 3, player_id: andrea.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 1, player_id: brandon.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 4, player_id: laura.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 2, player_id: sher.id)
+# --------- Week #2 ------------------------
+bw2 = bob.weeks.create(league_id: survivor.id, number: 2, score: 0)
 
-# Week 2 - Player Values - Andrea voted out
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 1, player_id: andrea.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 3, player_id: brandon.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 2, player_id: sher.id)
+# Team Choice - Bikal
+bw2.team_picks.create(team_id: gota.id,  picked: false)
+bw2.team_picks.create(team_id: bikal.id, picked: true)
 
-# Week 3 - Player Values - Sher voted out
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 2, player_id: brandon.id)
-cheech.player_picks.create(league_id: survivor.id, week: 1, value: 1, player_id: sher.id)
+# Player Values - Andrea voted out
+bw2.player_picks.create(player_id: andrea.id,  value: 3)
+bw2.player_picks.create(player_id: brandon.id, value: 1)
+bw2.player_picks.create(player_id: sher.id,    value: 2)
+
+# --------- Week #3 ------------------------
+bw3 = bob.weeks.create(league_id: survivor.id, number: 3, score: 0)
+
+# Player Values - Sher wins out
+bw3.player_picks.create(player_id: brandon.id, picked: true)
+bw3.player_picks.create(player_id: sher.id,    picked: false)
